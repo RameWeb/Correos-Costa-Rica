1334abff3f78e1122c652b596c81505bc0027ccf
NicoleRetana <nretanaa@ucenfotec.ac.cr>
2018-04-19 15:18:58 -0600

    Registro de sucursales BD

--- api/components/sucursales/sucursales.model.js
+++ api/components/sucursales/sucursales.model.js
@@ -5,12 +5,11 @@ let mongoose = require('mongoose');
 
 //Esquema de sucursales
 let sucursalesSchema = new mongoose.Schema({
-  idSucursal        : {type: String, required: true},
-  nombreSucursal              : {type: String, required: true},
-  nombreInstitucion  : {type: String, required: true},
+  idSucursal        : {type: String},
+  nombreSucursal              : {type: String},
   position             : {type: String},
-  direccion              : {type: String, required: true},
-  telefono              : {type: String, required: true},
+  direccion              : {type: String},
+  telefono              : {type: String},
 });
 //nombre del modelo dentro del back end y el userSchema es el nombre dentro de mongoose
 module.exports = mongoose.model('sucursales', sucursalesSchema);
